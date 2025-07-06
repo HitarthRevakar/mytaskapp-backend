09dd2548762aa48a20b4f1f6b5315ccc5c99c778
Hitarth Revakar <127825219+HitarthRevakar@users.noreply.github.com>
2025-07-06 21:31:54 +0530

    again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", `${FRONTEND_URL}`],
+  origin: ["http://localhost:5173", `${process.env.FRONTEND_URL}`],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
