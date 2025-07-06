69d5b536a02e6b22a61e1a842d22f07660cebfdc
Hitarth Revakar <127825219+HitarthRevakar@users.noreply.github.com>
2025-07-06 16:20:43 +0530

    bug fixes

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173",'https://mytaskapp-frontend.vercel.app/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+  origin: ["http://localhost:5173", "https://mytaskapp-frontend.vercel.app"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
+
 
 app.use(express.json());
 
