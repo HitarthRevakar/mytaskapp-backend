628ac035e5c65de6207cbb91997ac12a2ebda7e2
Hitarth Revakar <127825219+HitarthRevakar@users.noreply.github.com>
2025-07-07 12:03:27 +0530

    fix index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
 const express = require('express');
 const todoRoutes = require('./routes/todoRouter');
+const dotenv = require('dotenv');
 const app = express();
+dotenv.config();
+
+const FRONTEND_LOCAL = process.env.FRONTEND_URL_LOCAL;
+const FRONTEND_PRO = process.env.FRONTEND_URL_PRO
 
 // ------------ to connect frontend and backend ----------- //
 const cors = require('cors');
 app.use(cors({
-  origin: [process.env.FRONTEND_URL_LOCAL, process.env.FRONTEND_URL_PRO],
+  origin: [`${FRONTEND_LOCAL}`,`${FRONTEND_PRO}`].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
